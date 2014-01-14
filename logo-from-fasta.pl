d210d85c7cf0a25e59960956e84d684583ca00bb
Mike Covington <mfcovington@gmail.com>
2014-01-13 23:13:56 -0800

    Gitignore pdf and png files

--- logo-from-fasta.pl
+++ logo-from-fasta.pl
@@ -9,6 +9,7 @@ use warnings;
 use autodie;
 use feature 'say';
 use Data::Printer;
+use Statistics::R;
 
 my $base_dir = "/Users/mfc/git.repos/extract-seq-flanking-read/runs/out";
 # my $fasta_file = "$base_dir/A4_I1_5p14_sorted.10bp-upstream.fa";
@@ -51,6 +52,35 @@ my @r_cmd;
 push @r_cmd, "$_ <- c(", join( ", ", @{$out{$_}}), ")\n" for keys %out;
 print @r_cmd;
 
-# say "$_ <- c(", join( ", ", @{$out{$_}}), ")" for keys %out;
 
+my $build_pwm = <<EOF;
+# Adapted from http://davetang.org/muse/2013/01/30/sequence-logos-with-r/
 
+library(seqLogo)
+@r_cmd
+df <- data.frame(A,C,G,T)
+
+#define function that divides the frequency by the row sum i.e. proportions
+proportion <- function(x){
+  rs <- sum(x);
+  return(x / rs);
+}
+
+#create position weight matrix
+pwm <- apply(df, 1, proportion)
+pwm <- makePWM(pwm)
+EOF
+
+my ($base_name) = $fasta_file =~ m|([^/]+).fa(?:sta)?$|i;
+my $filetype = "pdf";
+
+my $write_logo = <<EOF;
+$filetype("$base_name.$filetype")
+seqLogo(pwm)
+dev.off()
+EOF
+
+my $R = Statistics::R->new();
+$R->run($build_pwm);
+$R->run($write_logo);
+$R->stop();
