0a461e9a06e55337b66e1f4b8a530f39311b351c
Mike Covington <mfcovington@gmail.com>
2014-01-13 21:47:37 -0800

    Add basic tool to re-format the width of a FASTA file

---
+++ format-fasta.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+# Mike Covington
+# created: 2013-12-24
+#
+# Description:
+#
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+
+my $fasta_file = $ARGV[0];
+my $fa_width //= 80;
+my $seqid;
+my $seq;
+
+open my $fasta_fh, "<", $fasta_file;
+while ( my $fa_line = <$fasta_fh>) {
+    if ($fa_line =~ /^>/) {
+        if ($seq) {
+            say $_ for unpack "(A$fa_width)*", $seq;
+            $seq = '';
+        }
+        print $fa_line;
+    }
+    else{
+        chomp $fa_line;
+        $seq .= $fa_line;
+    }
+}
+close $fasta_fh;
+
+say $_ for unpack "(A$fa_width)*", $seq;
+
+exit;
